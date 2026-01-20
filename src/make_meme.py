a56f943645fd9c2f8a142b30fdec125673eb5545
Himanshu Kumar <162711386+krHimanshu123@users.noreply.github.com>
2026-01-20 16:51:35 +0530

    Add default sentence for meme generation

--- src/make_meme.py
+++ src/make_meme.py
@@ -4,6 +4,7 @@ from inference import run_inference
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         sentence = " ".join(sys.argv[1:])
+        
     else:
         sentence = "When your code finally runs without errors"
 
